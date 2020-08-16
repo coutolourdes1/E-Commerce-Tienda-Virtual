ca58bae930133916153d0ed0c4e98e0df74c318b
coutolourdes <coutolourdes2020@gmail.com>
2020-08-16 17:46:37 -0300

    actualice login y cree lista de productos

--- js/login.js
+++ js/login.js
@@ -1,6 +1,20 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+function validarParametros() {
+    var valor_email = document.getElementById("email").value;
+    var valor_password = document.getElementById("password").value;
+    if(valor_email == ""){
+        document.getElementById("error1").innerHTML = "Ingrese datos";
+    }else if(valor_password == ""){
+        document.getElementById("error2").innerHTML = "Ingrese datos";
+    }else{
+        window.location.href = "principal.html";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
 });
+
+
