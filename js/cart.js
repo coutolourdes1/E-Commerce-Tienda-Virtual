032f9ed88ff59f8a2a6f60d66608386e662df71b
coutolourdes <coutolourdes2020@gmail.com>
2020-10-10 17:54:48 -0300

    diseno carrito de compras

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,64 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+//CONFIRMAR COMPRA 
 
+
+//mostrar JSON 
+var arrayCarrito = [];
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(PRODUCT2_CART).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            arrayCarrito = resultObj.data;
+            carritoDeCompras(arrayCarrito.articles);
+        }
+    })
 });
+
+function carritoDeCompras(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let carro = array[i];
+
+        if (carro.currency == "USD") {
+            carro.unitCost = carro.unitCost * 40;
+            carro.currency = "UYU";
+        }
+
+        htmlContentToAppend += `
+        <a class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + carro.src + `" class="img-thumbnail">
+                </div>
+                <div class="col-6">
+                    <h4 class="mb-1">` + carro.name + `</h4>
+                </div>
+                <div class="col-3"> 
+                    <p class="mb-1">` + carro.currency + " " + `<span name="costounit">` + carro.unitCost + `</span></p>
+                    <input onchange="precioTotal()" name="cantidad" type="number" value="` + carro.count + `"min="1" max="1000" step="1"></input>
+                </div>
+            </div> 
+        </a>
+        `
+        document.getElementById("cartProducts-list-container").innerHTML = htmlContentToAppend;
+
+    }
+    precioTotal();
+}
+
+function precioTotal() {
+    let cantidad = document.getElementsByName("cantidad");
+    let costo = document.getElementsByName("costounit");
+    let suma = 0;
+
+    let sumaprod1 = parseInt(costo[0].innerHTML) * parseInt(cantidad[0].value);
+    document.getElementById("totalProduct1").innerHTML = sumaprod1;
+
+    let sumaprod2 = parseInt(costo[1].innerHTML) * parseInt(cantidad[1].value);
+    document.getElementById("totalProduct2").innerHTML = sumaprod2;
+
+    for (let j = 0; j < cantidad.length; j++) {
+        suma += parseInt(costo[j].innerHTML) * parseInt(cantidad[j].value);
+    }
+    document.getElementById("subtotal").innerHTML = suma;
+}
--- js/product-info.js
+++ js/product-info.js
@@ -67,10 +67,51 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
         }
     });
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultComent) {
+        if (resultComent.status === "ok") {
+            arrayComentario = resultComent.data;
+            comentariosArray(arrayComentario);
+        }
+    });
+    getJSONData(PRODUCTS_URL).then(function(resultRelated) {
+        if (resultRelated.status === "ok") {
+            arrayProducts = resultRelated.data;
+            productosRelacionados(arrayProducts);
+        }
+    });
 });
+// productos relacionados 
+var arrayProducts = [];
 
-//carrusel de imagenes
-$('.carousel').carousel();
+function productosRelacionados(arrayP) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < arrayP.length; i++) {
+
+        let productos = arrayP[i];
+        if (i === 1 || i === 3) {
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + productos.imgSrc + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">` + productos.name + `</h4>
+                            <small class="text-muted">` + productos.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + productos.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+            document.getElementById("relatedproducts-list-container").innerHTML = htmlContentToAppend;
+
+        }
+    }
+}
 
 //comentarios
 var arrayComentario = [];
@@ -85,9 +126,9 @@ function comentariosArray(array) {
         htmlContentToAppend += `
         <div class="row">
                 <div class="col">
-                    <small class="text-muted"> Usuario: ` + comentarios.user + ` </small>
+                    <small class="text-muted"> <span class="far fa-user form-control-feedback"></span> ` + comentarios.user + `</small>
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">` + "Puntuación: " + comentarios.score + `</h5>
+                        <h5 class="mb-1">` + mostrarCalificacion(comentarios.score) + `</h5>
                         <small class="text-muted">` + comentarios.dateTime + ` </small>
                     </div>
                     <p class="mb-1">` + comentarios.description + `</p>
@@ -99,11 +140,18 @@ function comentariosArray(array) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(e) {
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj) {
-        if (resultObj.status === "ok") {
-            arrayComentario = resultObj.data;
-            comentariosArray(arrayComentario);
+function mostrarCalificacion(estrellas) {
+    cantidad = '';
+    for (let i = 0; i < 5; i++) {
+        if (i < estrellas) {
+            marcada = "checked";
+        } else {
+            marcada = "";
         }
-    });
-});
+
+        cantidad += '<span class="fa fa-star ' + marcada + '"></span>';
+    };
+    return cantidad;
+}
+
+//nuevo comentario
